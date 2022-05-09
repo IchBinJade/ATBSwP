7fc7e0c7d7c956632185f45df7ecf1dea667cfda
jadeh <jademhurst@gmail.com>
2022-05-09 19:59:05 +0100

    Initial commit

---
+++ character_picture_grid.py
@@ -0,0 +1,22 @@
+# Using the provided grid, write code that produces the following image:
+# ..OO.OO..
+# .OOOOOOO.
+# .OOOOOOO.
+# ..OOOOO..
+# ...OOO...
+# ....O....
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for x in range(len(grid[0])):
+    for y in range(len(grid)):
+        print(grid[y][x], end='')
+    print()
